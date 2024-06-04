08d7ec5fa24630270b60ea78c51f67c2dc1bffa5
Harry San Juan <harrycsanjuan@gmail.com>
2024-06-04 00:56:17 -0400

    add pokemon type validation for pokemon class

--- lib/models/pokemon.py
+++ lib/models/pokemon.py
@@ -10,4 +10,27 @@ class Pokemon:
         self.pokemon_type = pokemon_type
         self.level = level
         self.trainer_id = trainer_id
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError("Name must be a non-empty string")
+
+    @property
+    def pokemon_type(self):
+        return self._pokemon_type
+
+    @pokemon_type.setter
+    def pokemon_type(self, pokemon_type):
+        valid_types = ["Fire", "Water", "Grass"]
         
+        if pokemon_type in valid_types:
+            self._pokemon_type = pokemon_type
+        else:
+            raise ValueError("Please enter valid Pokemon Type: Fire, Water, or Grass")
