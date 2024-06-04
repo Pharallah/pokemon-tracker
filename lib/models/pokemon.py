9ceb16a04264f4c24280117c1600ecb795951503
Harry San Juan <harrycsanjuan@gmail.com>
2024-06-04 00:47:20 -0400

    Initialize Trainer and Pokemon classes

--- lib/models/pokemon.py
+++ lib/models/pokemon.py
@@ -1,4 +1,13 @@
 from models.__init__ import CONN, CURSOR
 
 class Pokemon:
-    pass
+    
+    all = {}
+
+    def __init__(self, name, pokemon_type, level=1, trainer_id, id=None):
+        self.id = id
+        self.name = name
+        self.pokemon_type = pokemon_type
+        self.level = level
+        self.trainer_id = trainer_id
+        
